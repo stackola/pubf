67657e5f3d1407d4d9255dbc5d9872bc1dd3378b
Willi Krappen <wkrappen91@yahoo.de>
2017-08-05 20:15:01 +0200

    types und so

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,11 @@
-console.log("tesst");
+var app = require('express')();
+var express = require('express');
+var http = require('http').Server(app);
+
+http.listen(process.env.PORT || 5000, function() {
+	console.log('listening on *:5000');
+});
+app.use(express.static('public'));
+
+
+var io = require('socket.io')(http);
